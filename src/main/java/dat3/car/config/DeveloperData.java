de5aab32bc5d01533cf084b0134d34ac0915a851
Ramilade <rami0904@stud.kea.dk>
2023-02-01 16:12:40 +0100

    Member and car repo added

--- src/main/java/dat3/car/config/DeveloperData.java
+++ src/main/java/dat3/car/config/DeveloperData.java
@@ -1,11 +1,39 @@
 package dat3.car.config;
 
+import dat3.car.entity.Car;
+import dat3.car.entity.Member;
+import dat3.car.repository.CarRepository;
+import dat3.car.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
 public class DeveloperData implements ApplicationRunner {
+  @Autowired
+  MemberRepository memberRepository;
+  @Autowired
+  CarRepository carRepository;
+
+
+  private final String passwordUsedByAll = "test12";
+
   @Override
   public void run(ApplicationArguments args) throws Exception {
+    Member m1 = new Member("member1", passwordUsedByAll, "memb1@a.dk", "Kurt", "Wonnegut", "Lyngbyvej 2", "Lyngby", "2800");
+    Member m2 = new Member("member2", passwordUsedByAll, "aaa@dd.dk", "Hanne", "Wonnegut", "Lyngbyvej 2", "Lyngby", "2800");
+    memberRepository.save(m1);
+    memberRepository.save(m2);
+
+    Car c1 = new Car("Audi","Q4",200.0, 10);
+    Car c2 = new Car();
+    Car c3 = new Car();
+
+    carRepository.save(c1);
 
   }
 }
+
+
